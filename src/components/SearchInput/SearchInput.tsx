e41372771cbe0083548917ea2370ecadc97b7f09
OJ Abba <phavorabba@gmail.com>
2025-02-03 15:22:59 +0000

    [Feat] Create general UI components

---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,61 @@
+import { faSearch, faTimes, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Input, IconButton } from "@deskpro/deskpro-ui";
+import { useState, useEffect } from "react";
+import Label from "../Label";
+import type { FC, ChangeEvent } from "react";
+
+type SearchInputProps = {
+  label?: string,
+  onChange?: (search: string) => void,
+  disabled?: boolean,
+  required?: boolean,
+  isFetching?: boolean,
+};
+
+const SearchInput: FC<SearchInputProps> = ({
+  label,
+  onChange,
+  disabled = false,
+  required = false,
+  isFetching = false,
+}) => {
+  const [search, setSearch] = useState<string>("")
+
+  const onChangeSearch = ({ target: { value: q }}: ChangeEvent<HTMLInputElement>) => {
+    setSearch(q);
+  }
+
+  const onClearSearch = () => {
+    setSearch("");
+  }
+
+  useEffect(() => {
+    onChange && onChange(search);
+  }, [search, onChange]);
+
+  return (
+    <Label
+      required={required}
+      label={label}
+      htmlFor="search"
+    >
+      <Input
+        id="search"
+        name="search"
+        value={search}
+        disabled={disabled}
+        onChange={onChangeSearch}
+        leftIcon={isFetching
+          ? <FontAwesomeIcon icon={faSpinner} spin/>
+          : faSearch
+        }
+        rightIcon={(
+          <IconButton icon={faTimes} minimal onClick={onClearSearch} />
+        )}
+      />
+    </Label>
+  );
+}
+
+export default SearchInput;
---
+++ src/components/TwoColumnNavigation/TwoColumnNavigation.tsx
@@ -0,0 +1,28 @@
+import { faSearch, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { TwoButtonGroup, TwoButtonGroupProps } from "@deskpro/app-sdk";
+import { FC } from "react";
+
+type Props = {
+  selected: TwoButtonGroupProps["selected"],
+  onOneNavigate?: TwoButtonGroupProps["oneOnClick"],
+  onTwoNavigate?: TwoButtonGroupProps["twoOnClick"],
+}
+
+const TwoColumnNavigation: FC<Props> = ({ selected, onOneNavigate, onTwoNavigate }) => {
+
+    const discardFunc = ()=>{}
+    
+  return (
+    <TwoButtonGroup
+      selected={selected}
+      oneLabel="Find Task"
+      twoLabel="Create Task"
+      oneIcon={faSearch}
+      twoIcon={faPlus}
+      oneOnClick={onOneNavigate || discardFunc}
+      twoOnClick={onTwoNavigate || discardFunc}
+    />
+  )
+}
+
+export default TwoColumnNavigation 
