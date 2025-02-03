324e5ed974c6176408e13d1d569980910893e664
OJ Abba <phavorabba@gmail.com>
2025-02-03 15:17:27 +0000

    [Feat] Create `Option` type

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,3 +1,7 @@
+import { DropdownValueType } from "@deskpro/deskpro-ui";
+
 export type Maybe<T> = T | undefined | null;
 
 export type Dict<T> = Record<string, T>;
+
+export type Option<Value = unknown> = Omit<DropdownValueType<Value>, "subItems">;
