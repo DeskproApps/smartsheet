ac25e63e26d0495cc485dcc01041a18fe1846d9b
OJ Abba <phavorabba@gmail.com>
2025-02-04 14:35:39 +0000

    [Feat] Create `getRegisteredTaskIds` function

---
+++ src/api/deskpro/getRegisteredTaskIds.ts
@@ -0,0 +1,9 @@
+import { IDeskproClient } from "@deskpro/app-sdk";
+
+export default async function getRegisteredTaskIds(client?: IDeskproClient, ticketId?: string): Promise<number[]> {
+    if (!ticketId || !client) return []
+
+    const linkedIds = await client.getEntityAssociation("linkedSmartsheetTasks", ticketId).list();
+    const taskIds = linkedIds.map(Number);
+    return taskIds;
+}
