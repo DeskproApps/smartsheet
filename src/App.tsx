c03c8da473644aa136dd086f7c212a73fbde533b
OJ Abba <phavorabba@gmail.com>
2025-02-07 13:47:11 +0000

    [Chore] Add new pages to `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+import AdminCallbackPage from "@/pages/AdminCallbackPage";
 import HomePage from "@/pages/HomePage";
 import LinkTasksPage from "@/pages/LinkTasksPage/LinkTasksPage";
+import LoginPage from "@/pages/LoginPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import RedirectPage from "@/pages/RedirectPage";
 import ViewTaskPage from "@/pages/ViewTaskPage";
@@ -8,11 +10,13 @@ import ViewTaskPage from "@/pages/ViewTaskPage";
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<HomePage />} />
-      <Route path={"/sheet/:sheetId/rows/:rowId"} element={<ViewTaskPage />} />
-      <Route path={"/rows/link"} element={<LinkTasksPage />} />
       <Route index element={<RedirectPage />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route path="/admin/callback" element={<AdminCallbackPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/rows/link" element={<LinkTasksPage />} />
+      <Route path="/sheet/:sheetId/rows/:rowId" element={<ViewTaskPage />} />
     </Routes>
   );
 }
