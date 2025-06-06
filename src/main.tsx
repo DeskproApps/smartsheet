19dd4ff03b769fc0d63a727d3470e8ebec26d6af
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-06-06 11:59:47 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#28)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
+import './instrument';
 import "./main.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "simplebar/dist/simplebar.min.css";
 import { App } from "./App";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
-import { ErrorBoundary } from "react-error-boundary";
 import { HashRouter } from "react-router-dom";
 import { queryClient } from "@/query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Scrollbar } from "@deskpro/deskpro-ui";
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render((
   <StrictMode>
     <Scrollbar style={{ height: "100%", width: "100%" }} autoHide>
